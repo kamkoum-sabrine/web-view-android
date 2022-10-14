6f81a3feced5cd6188cd7e3efe1926a32ef4f56c
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-10-14 20:16:14 +0200

    first commit

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    lateinit var webview : WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webview = findViewById(R.id.webview)
+        webview.webViewClient = WebViewClient()
+        webview.loadUrl("https://google.com")
+    }
+}
